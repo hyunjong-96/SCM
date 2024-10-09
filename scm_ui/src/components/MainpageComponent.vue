469a8d91ac3e793af56071b0efab6706e434cdcd
hyunjong-96 <ahajongs@gmail.com>
2024-10-09 22:29:44 +0900

    init - scm_ui 추가

---
+++ scm_ui/src/components/MainpageComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <!-- <v-row class="text-center">
+            <h1 class="display-2 font-weight-bold mb-3">
+                Welcome to the Vuetify 3 Beta
+            </h1>
+        </v-row> -->
+        <h1 >
+                Hello custom login
+            </h1>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+    name: 'MainpageComponent',
+    data: () => {
+
+    }
+}
+
+</script>
---
+++ scm_ui/src/views/MainpageView.vue
@@ -0,0 +1,15 @@
+<template>
+    <MainpageComponent/>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import MainpageComponent from '../components/MainpageComponent.vue'
+
+export default defineComponent({
+    name: 'MainpageView',
+    components: {
+        MainpageComponent
+    }
+})
+</script>
