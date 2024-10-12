8ca6ef8b9bb283a14fb6bd06a2294355ac265a00
hyunjong-96 <ahajongs@gmail.com>
2024-10-12 17:06:32 +0900

    feat : WebConfig 추가 - index.html resolver 추가

---
+++ scm_api/src/main/java/com/scm/api/controller/CustomPathResourceResolver.java
@@ -0,0 +1,18 @@
+package com.scm.api.controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+public class CustomPathResourceResolver extends PathResourceResolver {
+
+    @Override
+    public Resource getResource(String resourcePath, Resource location) throws IOException {
+        Resource requestResource = location.createRelative(resourcePath);
+
+        return requestResource.exists() && requestResource.isReadable()
+                ? requestResource : new ClassPathResource("/static/index.html");
+    }
+}
